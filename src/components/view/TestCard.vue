962f294cd624ab78cd2afea62616c5ba4e8a1329
aldahodzic <aldahodzic844@gmail.com>
2022-01-25 12:56:02 -0500

    added basic record txt export function

--- src/components/view/TestCard.vue
+++ src/components/view/TestCard.vue
@@ -12,6 +12,43 @@ export default {
         await this.$store.dispatch('clearApp')
       }
     },
+
+    basicExport() {
+      if (confirm('Download all fitness records?')) {
+        this.downloadFile(
+          'workouts.txt',
+          JSON.stringify(this.$store.getters['workouts/getState'])
+        )
+        this.downloadFile(
+          'exercises.txt',
+          JSON.stringify(this.$store.getters['exercises/getState'])
+        )
+        this.downloadFile(
+          'workoutRecords.txt',
+          JSON.stringify(this.$store.getters['workoutRecords/getState'])
+        )
+        this.downloadFile(
+          'exerciseRecords.txt',
+          JSON.stringify(this.$store.getters['exerciseRecords/getState'])
+        )
+      }
+    },
+
+    downloadFile(filename, textInput) {
+      let tempElement = document.createElement('a')
+      tempElement.setAttribute(
+        'href',
+        'data:text/plain;charset=utf-8, ' + encodeURIComponent(textInput)
+      )
+      tempElement.setAttribute('download', filename)
+      document.body.appendChild(tempElement)
+      tempElement.click()
+      document.body.removeChild(tempElement)
+    },
+
+    advancedExport() {
+      confirm('Not Implemented')
+    },
   },
 }
 </script>
@@ -23,12 +60,20 @@ export default {
 
       <v-card-actions>
         <v-container>
-          <v-btn color="success mr-3" @click="useDefaults()">
+          <v-btn color="success mr-3 mb-3" @click="useDefaults()">
             Set Defaults
           </v-btn>
 
-          <v-btn color="error mr-3" @click="clearApp()">
+          <v-btn color="error mr-3 mb-3" @click="clearApp()">
             Clear App
+          </v-btn>
+
+          <v-btn color="warning mr-3 mb-3" @click="basicExport()">
+            Basic Export
+          </v-btn>
+
+          <v-btn color="warning mr-3 mb-3" @click="advancedExport()">
+            Advanced Export
           </v-btn>
         </v-container>
       </v-card-actions>
